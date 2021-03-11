de6e553aa88a86d75ef0e0f4cc87002867bf500e
vivimortensen <vivi4531@stud.kea.dk>
2021-03-11 11:27:40 +0100

    1103 gsap exercise 2 and 3 done

--- beachball.js
+++ beachball.js
@@ -1,4 +1,12 @@
 "use strict";
+/* //EXERCISE 1
 gsap.to(".ball", {x:400, duration: 2});
 gsap.to(".ball", {rotate:200, duration: 2});
-gsap.to(".ball", {scale:0, y:25, duration: 2, delay: 2});
+gsap.to(".ball", {scale:0, y:25, duration: 2, delay: 2}); */
+
+//EXERCISE 3
+const timeline = gsap.timeline();
+
+timeline.to(".ball", {duration: 5, rotation: 360, x:400});
+timeline.to(".ball", {duration: 4, x:"100vw", y:"-100vh"});
+timeline.to(".ball", {duration: 8, x:0, y:0});
--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,5 @@
 "use strict";
+//EXERCISE 2
+gsap.to(".ball", {y:400, duration: 2, stagger: 0.2});
+gsap.to(".ball", { duration: 2, ease: "bounce", y: 400 });
+
